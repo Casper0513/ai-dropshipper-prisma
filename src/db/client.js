f425926d7c3975efab78a86e8ed16313975a5f71
Casper0513 <brian_cass102@hotmail.com>
2025-12-11 15:38:03 -0700

    Prisma changes

--- src/db/client.js
+++ src/db/client.js
@@ -1,17 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPostgreSQL } from "@prisma/adapter-postgresql";
+
+const adapter = new PrismaPostgreSQL(process.env.DATABASE_URL);
 
 let prisma;
 
 if (!globalThis.__prisma) {
-  prisma = new PrismaClient({
-    errorFormat: "minimal"
-  });
-
-  if (process.env.NODE_ENV !== "production") {
-    globalThis.__prisma = prisma;
-  }
+  prisma = new PrismaClient({ adapter });
+  globalThis.__prisma = prisma;
 } else {
   prisma = globalThis.__prisma;
 }
 
 export { prisma };
+
