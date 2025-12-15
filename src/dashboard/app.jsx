73ceb2d83eece569d4f5aa0fa0594339172e074a
Casper0513 <brian_cass102@hotmail.com>
2025-12-14 19:46:47 -0700

    added upgraded dashboard files

--- src/dashboard/app.jsx
+++ src/dashboard/app.jsx
@@ -1,6 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import DashboardApp from "./Dashboard.jsx";
+import React from "react";
+import "./dashboard.css";
+
+export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<DashboardApp />);
